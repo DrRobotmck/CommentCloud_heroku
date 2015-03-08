ae908ef886e8107688b8d4adbb1a4abf3df39adb
DrRobotmck <mckenneth.scott@icloud.com>
2015-03-08 00:21:40 -0500

    added unfollow button if already following artist

--- app/assets/javascripts/backbone/views/trackView.js
+++ app/assets/javascripts/backbone/views/trackView.js
@@ -5,23 +5,34 @@ var TrackView = Backbone.View.extend({
 		this.listenTo(this.model, 'change', this.render);
 	},
 	render: function() {
+		this.userID = this.model.get('origin').user_id;
 		this.$el.html(HandlebarsTemplates['track'](this.model.toJSON()));
+		this.checkIfFollowing();
 	},
 	events: {
 		'click .follow': 'followArtist',
 		'click .unfollow': 'unfollowArtist'
 	},
-	followArtist: function () {
-		var userID = this.model.get('origin').user_id;
-		SC.put('/me/followings/' + userID, function (user, error) {
+	checkIfFollowing: function() {
+		var userID = this.userID;
+		SC.get('/me/followings/', function(result, error) {
+			var followings = result.map(function(user) { return user.id });
+			if (followings.indexOf(userID) !== -1) {
+				$('.follow').toggleClass('follow')
+										.toggleClass('unfollow')
+										.text('Unfollow');
+			}
+		});
+	},
+	followArtist: function() {
+		SC.put('/me/followings/' + this.userID, function(user, error) {
 			$('.follow').toggleClass('follow')
 									.toggleClass('unfollow')
 									.text('You are now following ' + user.username);
 		});
 	},
-	unfollowArtist: function () {
-		var userID = this.model.get('origin').user_id;
-		SC.delete('/me/followings/' + userID, function (user, error) {
+	unfollowArtist: function() {
+		SC.delete('/me/followings/' + this.userID, function(user, error) {
 			$('.unfollow').toggleClass('follow')
 										.toggleClass('unfollow')
 										.text('follow artist');
